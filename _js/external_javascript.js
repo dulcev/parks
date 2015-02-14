59ac76093eaa2bce6ac5f82d37722ba6cd867f40
Dulce Velasco <KandyRainbowBright@Dulces-MBP-5.home>
2015-02-13 20:04:24 -0800

    added 2 slideshows and modified photos

---
+++ _js/external_javascript.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+setTimeout(slide, 2000);
+
+function slide() {
+var fistImg = $("#horizontal .slides .drawer img:first");
+
+fistImg.animate({"margin-left":"-450px"},1000, function (){
+fistImg.appendTo("#horizontal .slides .drawer");
+fistImg.css({"margin-left":"0px"});
+
+setTimeout(slide, 2000);
+
+});
+}
+});
+
+
+
+$(document).ready(function () {
+console.log('here we go');
+				
+setTimeout(slide, 2000);
+				
+//play
+function slide() {
+				
+//set and get
+var firstIMG = $('#vertical .slides img:first');
+
+//function
+firstIMG.animate({"margin-top":"-400px"},1000, function () {
+// console.log('I am working');
+firstIMG.appendTo('#vertical .slides');
+firstIMG.css({"margin-top":"0px"});
+
+// slide();
+setTimeout(slide, 2000);
+});
+
+//triggers
+}
+});
